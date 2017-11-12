33707077be13ba41f807c70dd02c35cf17b688d5
Evan Bacon <baconbrix@gmail.com>
2017-11-11 17:12:41 -0800

    updated image - crashes now!

--- components/Scene.js
+++ components/Scene.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Expo from 'expo';
-import { Dimensions, Image, StyleSheet, Platform } from 'react-native'
-import Touches from '../window/Touches';
-import Files from '../Files';
-import { PixiView } from './index';
-import * as PIXI from 'pixi.js';
+import React from "react";
+import Expo from "expo";
+import { Dimensions, Image, StyleSheet, Platform } from "react-native";
+import Touches from "../window/Touches";
+import Files from "../Files";
+import { PixiView } from "./index";
+import * as PIXI from "pixi.js";
 // var PixiGL = require('pixi-gl-core');
 
 export const createTextureAsync = async ({ asset }) => {
   if (!asset.localUri) {
     await asset.downloadAsync();
   }
-  return ({
+  return {
     data: asset,
     width: asset.width,
-    height: asset.height,
-  });
+    height: asset.height
+  };
 };
 
 const textStyle = { fill: 0xffffff };
@@ -31,8 +31,8 @@ function createSprite(texture, text) {
 
 class Scene extends React.Component {
   static defaultProps = {
-    onFinishedLoading: (() => { })
-  }
+    onFinishedLoading: () => {}
+  };
   shouldComponentUpdate = () => false;
 
   render = () => (
@@ -43,7 +43,7 @@ class Scene extends React.Component {
     />
   );
 
-  onContextCreateAsync = async (gl) => {
+  onContextCreateAsync = async gl => {
     const { width, height, scale } = Dimensions.get("window");
 
     /*
@@ -52,14 +52,14 @@ class Scene extends React.Component {
     */
 
     const app = new PIXI.Application({
-      width, 
+      width,
       height,
       context: gl,
       resolution: scale,
       backgroundColor: 0x1099bb,
       powerPreference: "high-performance",
       legacy: true,
-      forceCanvas: false,
+      forceCanvas: false
     });
 
     /*
@@ -67,52 +67,24 @@ class Scene extends React.Component {
       Textures aren't working
       when they do we should see Nik
     */
-    
+
     // create a new Sprite from an image path
     const asset = Expo.Asset.fromModule(Files.sprites.nik);
-    const texture = new PIXI.BaseTexture.from(asset.localUri);
-    const benny = PIXI.Sprite.from(texture);
-    // const benny = PIXI.Sprite.fromImage(asset.localUri)
-    benny.backgroundColor = 0xfff000;
-    // center the sprite's anchor point
-    benny.anchor.set(0.5);
-    // move the sprite to the center of the screen
-    benny.x = width / 2;
-    benny.y = height / 2;
+    if (!asset.localUri) await asset.downloadAsync();
 
-    console.warn("", benny.getLocalBounds(), benny.texture.orig);
-
-    
-    this.benny = benny;
-    // /*
-    //   Draw some random Graphics
-    // */
-    const graphics = new PIXI.Graphics();
-    // set a fill and line style
-    graphics.beginFill(0xFF3300);
-    graphics.lineStyle(2, 0x0000FF, 1);
-    graphics.beginFill(0xFF700B, 1);
-    graphics.drawRect(0, 0, width, height);
-    graphics.endFill();
-    this.graphics = graphics;
-    
-    benny.addChild(graphics);
-    
+    const benny = PIXI.Sprite.from(new HTMLImageElement(asset));
     app.stage.addChild(benny);
 
-    window.addEventListener('resize', this.onWindowResize, false);
+    window.addEventListener("resize", this.onWindowResize, false);
 
     this.props.onFinishedLoading && this.props.onFinishedLoading();
   };
 
-  animate = (delta) => {
-    this.benny.rotation += 0.1 * delta;
-  }
+  animate = delta => {};
 
   onWindowResize = () => {
     const { width, height, scale } = Dimensions.get("window");
-  }
-
+  };
 }
 
 export default Touches(Scene);
--- window/domElement.js
+++ window/domElement.js
@@ -1,122 +1,117 @@
-
-import { Image, Dimensions } from 'react-native';
-import EventEmitter from 'EventEmitter';
+import { Image, Dimensions } from "react-native";
+import EventEmitter from "EventEmitter";
 
 class DOMNode {
-    constructor(nodeName) {
-        this.nodeName = nodeName;
-    }
+  constructor(nodeName) {
+    this.nodeName = nodeName;
+  }
 
-    get ownerDocument() {
-        return window.document;
-    }
+  get ownerDocument() {
+    return window.document;
+  }
 
-    appendChild(element) {
-        // unimplemented
-    }
+  appendChild(element) {
+    // unimplemented
+  }
 }
 
 class DOMElement extends DOMNode {
-    style = {};
-    emitter = new EventEmitter();
-    constructor(tagName) {
-        return super(tagName.toUpperCase());
-    }
+  style = {};
+  emitter = new EventEmitter();
+  constructor(tagName) {
+    return super(tagName.toUpperCase());
+  }
 
-    get tagName() {
-        return this.nodeName;
-    }
+  get tagName() {
+    return this.nodeName;
+  }
 
-    insertBefore = () => {
+  insertBefore = () => {};
+  getContext(contextType) {
+    // if (global.canvasContext) {
+    //   return global.canvasContext;
+    // }
+    return {
+      fillRect: _ => {},
+      drawImage: _ => {},
+      getImageData: _ => {},
+      getContextAttributes: _ => ({
+        stencil: true
+      }),
+      getExtension: _ => ({
+        loseContext: _ => {}
+      })
+    };
+  }
 
-    }
-    getContext(contextType) {
-        // if (global.canvasContext) {
-        //   return global.canvasContext;
-        // }
-        return {
-            fillRect: (_ => { }),
-            drawImage: (_ => { }),
-            getImageData: (_ => { }),
-            getContextAttributes: (_ => ({
-                stencil: true
-            })),
-            getExtension: (_ => ({
-                loseContext: (_ => {
+  addEventListener(eventName, listener) {
+    this.emitter.addListener(eventName, listener);
+  }
 
-                })
-            })),
-        }
-    }
+  removeEventListener(eventName, listener) {
+    this.emitter.removeListener(eventName, listener);
+  }
 
-    addEventListener(eventName, listener) {
-        this.emitter.addListener(eventName, listener)
-    }
+  get clientWidth() {
+    return this.innerWidth;
+  }
+  get clientHeight() {
+    return this.innerHeight;
+  }
 
-    removeEventListener(eventName, listener) {
-        this.emitter.removeListener(eventName, listener)
-    }
-
-    get clientWidth() {
-        return this.innerWidth;
-    }
-    get clientHeight() {
-        return this.innerHeight;
-    }
-
-    get innerWidth() {
-        return window.innerWidth;
-    }
-    get innerHeight() {
-        return window.innerHeight;
-    }
+  get innerWidth() {
+    return window.innerWidth;
+  }
+  get innerHeight() {
+    return window.innerHeight;
+  }
 }
 
 class DOMDocument extends DOMElement {
-    body = new DOMElement('BODY');
+  body = new DOMElement("BODY");
 
-    constructor() {
-        super('#document');
-    }
+  constructor() {
+    super("#document");
+  }
 
-    createElement(tagName) {
-        return new DOMElement(tagName);
-    }
+  createElement(tagName) {
+    return new DOMElement(tagName);
+  }
 
+  createElementNS(tagName) {
+    const canvas = this.createElement(tagName);
+    canvas.getContext = () => ({
+      fillRect: _ => ({}),
+      drawImage: _ => ({}),
+      getImageData: _ => ({}),
+      getContextAttributes: _ => ({
+        stencil: true
+      }),
+      getExtension: _ => ({
+        loseContext: _ => ({})
+      })
+    });
+    canvas.toDataURL = _ => ({});
 
-    createElementNS(tagName) {
-        const canvas = this.createElement(tagName);
-        canvas.getContext = () => ({
-            fillRect: (_ => ({})),
-            drawImage: (_ => ({})),
-            getImageData: (_ => ({})),
-            getContextAttributes: (_ => ({
-                stencil: true
-            })),
-            getExtension: (_ => ({
-                loseContext: (_ => ({
+    return canvas;
+  }
 
-                }))
-            })),
-        })
-        canvas.toDataURL = (_ => ({}))
-
-        return canvas;
-    }
-
-
-    getElementById(id) {
-        return new DOMElement('div');
-    }
+  getElementById(id) {
+    return new DOMElement("div");
+  }
 }
 
-process.browser = true
+process.browser = true;
 
 window.emitter = window.emitter || new EventEmitter();
-window.addEventListener = window.addEventListener || ((eventName, listener) => window.emitter.addListener(eventName, listener));
-window.removeEventListener = window.removeEventListener || ((eventName, listener) => window.emitter.removeListener(eventName, listener));
+window.addEventListener =
+  window.addEventListener ||
+  ((eventName, listener) => window.emitter.addListener(eventName, listener));
+window.removeEventListener =
+  window.removeEventListener ||
+  ((eventName, listener) => window.emitter.removeListener(eventName, listener));
 window.document = new DOMDocument();
-window.document.body = new DOMElement('body');
+window.document.body = new DOMElement("body");
 window.location = "data:"; // <- Not sure about this... or anything for that matter ¯\_(ツ)_/¯
 global.userAgent = global.navigator.userAgent = "iPhone"; // <- This could be made better, but I'm not sure if it'll matter for PIXI
 global.HTMLVideoElement = global.HTMLCanvasElement = () => ({});
@@ -124,39 +119,18 @@ global.document = window.document;
 
 /// I'm just guessing now
 class CustomImage extends Image {
-    constructor(width, height) {
-        super();
-    }
-    set src(val) {
-        this.source = val;
-        if (typeof val === 'string') {
-            Image.prefetch(val);
-            this.source = { uri: val };
-        } else {
-            let asset = Expo.Asset.fromModule(val);
-
-            if (!asset.localUri) {
-                (async () => {
-                    await asset.downloadAsync();
-                })()
-            }
-        }
-    }
+  constructor(props) {
+    super();
+    this.align = "center";
+    this.alt = null;
+    this.border = null;
+    this.complete = true;
+    this.crossOrigin = "";
+    this.lowSrc = this.currentSrc = this.src = props.localUri;
+    this.width = props.width;
+    this.height = props.height;
+    this.isMap = true;
+  }
 }
 global.HTMLImageElement = global.Image = CustomImage;
 global.performance = null;
-
-/// Old version
-
-// global.Image = (width = 0, height = 0) => {
-//   let img = new DOMElement('IMG');;
-//   img.width = width
-//   img.height = height
-//   // img.src = "";
-//   if (!asset.localUri) {
-//     await asset.downloadAsync();
-//   }
-//   set src = () => {
-//   }
-//   return img;
-// };
